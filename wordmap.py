b4facc2b69bba3077129cb0b44630ded7e708229
Maria Malasiewicz <malasie@users.noreply.github.com>
2023-05-20 18:49:58 +0200

    heh
    something before i figured out what i have to do...

--- wordmap.py
+++ wordmap.py
@@ -36,7 +36,7 @@ def text_preparation(text, language='english'):
         
     text = nlp(text)
 
-    unwanted_pos = ["sym", "num", "punct", "intj", "aux"]
+    unwanted_pos = ["SYM", "NUM", "PUNCT", "INTJ", "AUX"]
     words=[]
     for token in text:
         if len(token)>1:
@@ -47,6 +47,8 @@ def text_preparation(text, language='english'):
     
     return words
 
+
+#%%
 def plot(pdf_path, language='english', filename='', path=''):
     
     information, number_of_pages = extract_information(pdf_path)
@@ -71,35 +73,57 @@ def plot(pdf_path, language='english', filename='', path=''):
                 freq[word][i]+=1
                 
     wspol={}
-    max_freq=sum(max(freq.values()))
+    max_freq = max(map(max, freq.values()))
+    print(max_freq)
+    lim= 100
+    sum_freq = np.array(list(map(sum, freq.values())))
+    sum_freq = sum_freq[sum_freq>0.1*max(sum_freq)]
+    mean = np.mean(sum_freq)
+    directions=[[-1,1],[1,1],[1,-1],[-1,-1]]
+
     for word in freq.keys():
-        x=10
-        y=10
-        
-        if freq[word][0]>max_freq*0.05:
-            x -= (11-freq[word][0]/max_freq)
-            y += (11-freq[word][0]/(max_freq))
-            
-        if freq[word][1]>max_freq*0.05:
-            x += (11-freq[word][1]/(max_freq))
-            y += (11-freq[word][1]/(max_freq))
-            
-        if freq[word][2]>max_freq*0.05:
-            x += (11-freq[word][2]/(max_freq))
-            y -= (11-freq[word][2]/(max_freq))
-        if freq[word][3]>max_freq*0.05:
-            x -= (11-freq[word][3]/(max_freq))
-            y -= (11-freq[word][3]/(max_freq))
-            
-        if x>20:
-            x=20
-        if y>20:
-            y=20
-        if x<0:
+        #grupa startowa
+        if sum(freq[word])>max(sum_freq)*0.10:
             x=0
-        if y<0:
             y=0
-        wspol[word]=[x,y]
+            for i in range(4):
+                x += directions[i][0]*freq[word][i]
+                y += directions[i][1]*freq[word][i]
+            
+          
+            if sum(freq[word])>mean:
+                if sum(freq[word])>np.mean(sum_freq[sum_freq>mean]):
+                    x = 20/sum(freq[word])
+                    y = 20/sum(freq[word])
+                else:
+                    x = 100/sum(freq[word])
+                    y = 100/sum(freq[word])
+            else:
+                x = 200/sum(freq[word])
+                y = 200/sum(freq[word])
+            
+            if abs(x)>lim or abs(y)>lim:
+                print('Potrzebne')
+            if x>lim:
+                x= lim
+            elif x<-lim:
+                x=-lim
+            if y> lim:
+                y= lim
+            elif y<-lim:
+                y= -lim
+            wspol[word]=[x,y]
+               
+        
+'''
+come [22, 23, 17, 16] -2 -12
+day [13, 18, 34, 19] -20 22
+dog [4, 35, 0, 3] -28 -36
+say [44, 107, 65, 49] -79 -37
+little [33, 35, 26, 15] -13 -27
+time [20, 27, 54, 34] -27 41
+'''  
+        
         
 
     x = np.transpose(list(wspol.values()))[0]
@@ -108,14 +132,19 @@ def plot(pdf_path, language='english', filename='', path=''):
 
     fig, ax = plt.subplots()
     ax.scatter(x,y)
-
+    
     for i, txt in enumerate(n):
         ax.annotate(txt, (x[i], y[i]))
     
+    plt.xlim(-lim, lim)
+    plt.ylim(-lim, lim)
     plt.show()
     
+    return freq,wspol
 
-        
+freq,wspol=plot('The Stranger - Albert Camus.pdf')        
+
+#%%
 
 def wordCloud(word_list, info='', filename='', path='', width = 500, height = 300, background = "white",
                colormap = "magma"):
